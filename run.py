4f6549a02b20f48355258e52aee98253c5efe969
eclipsejenny <yqiu35@gmail.com>
2018-06-15 14:56:28 -0400

    Run project from Python subprocess.call

---
+++ run.py
@@ -0,0 +1,14 @@
+from subprocess import call
+
+print("Execute run.py")
+call(["mvn", "clean"])
+call(["mvn", "compile"])
+
+
+#call(["mvn", "exec:java", "-Dexec.args='train FanShare-ID-train'"])
+call(["mvn", "exec:java", "-Dexec.args='train train'"])
+#call(["mvn", "exec:java", "-Dexec.args='train train-short'"])
+
+#call(["mvn", "exec:java", "-Dexec.args='eval FanShare-ID-test'"])
+call(["mvn", "exec:java", "-Dexec.args='eval Tweet2013-test'"])
+#call(["mvn", "exec:java", "-Dexec.args='eval train-short'"])
